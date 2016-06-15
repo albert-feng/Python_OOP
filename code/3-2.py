a7ce315cb97850fdb26ff85c46c3cb60a6a26abf
albert-feng <fwg1989@gmail.com>
2016-06-15 23:13:47 +0800

    update sample code

--- code/3-2.py
+++ code/3-2.py
@@ -10,9 +10,14 @@ class Programer(object):
         self._age = age
         self.__weight = weight
 
+    def get_weight(self):
+        return self.__weight
+
 
 if __name__ == '__main__':
     programer = Programer('Albert', 25, 80)
     print dir(programer)
     print programer.__dict__
+    print programer.get_weight()
+    print programer._Programer__weight
 
