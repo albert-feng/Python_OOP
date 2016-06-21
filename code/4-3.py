11d4bf4ba5584257312e6725506f58b6c0969927
albert-feng <fwg1989@gmail.com>
2016-06-21 22:25:56 +0800

    add code and ppt for 4-3

---
+++ code/4-3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Programer(object):
+
+    def __init__(self, name, age):
+        self.name = name
+        if isinstance(age, int):
+            self.age = age
+        else:
+            raise Exception('age must be int')
+
+    def __eq__(self, other):
+        if isinstance(other, Programer):
+            if self.age == other.age:
+                return True
+        else:
+            raise Exception('The type of object must be Programer')
+
+    def __add__(self, other):
+        if isinstance(other, Programer):
+            return self.age + other.age
+        else:
+            raise Exception('The type of object must be Programer')
+
+
+if __name__ == '__main__':
+    p1 = Programer('Albert', 25)
+    p2 = Programer('Bill', 30)
+    print p1 == p2
+    print p1 + p2
