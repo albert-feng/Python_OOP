654c7dabba6c4842c8c1c899cfac39f86545bd4b
albert-feng <fwg1989@gmail.com>
2016-06-22 23:14:05 +0800

    add all ppt and code

--- code/4-5.py
+++ code/4-5.py
@@ -8,13 +8,14 @@ class Programer(object):
         self.name = name
         self.age = age
 
-    def __getattr__(self, key):
-        # return self.name
-        return self.__dict__[key]
+    def __getattribute__(self, name):
+        # return getattr(self, name)
+        # return self.__dict__[name]
+        return super(Programer, self).__getattribute__(name)
 
-    def __setattr__(self, key, value):
-        # self.name = value
-        self.__dict__[key] = value
+    def __setattr__(self, name, value):
+        # setattr(self, name, value)
+        self.__dict__[name] = value
 
 if __name__ == '__main__':
     p = Programer('Albert', 25)
